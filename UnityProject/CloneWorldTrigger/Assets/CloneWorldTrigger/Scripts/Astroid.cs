d95b8dc4a54639ff288309ee292519a20bc5337f
kazushi sakashita <kazusi@hotmail.co.jp>
2019-02-03 17:47:30 +0900

    [update] 練習と自作プロダクトを区分

--- UnityProject/CloneWorldTrigger/Assets/CloneWorldTrigger/Scripts/Astroid.cs
+++ UnityProject/CloneWorldTrigger/Assets/CloneWorldTrigger/Scripts/Astroid.cs
@@ -8,7 +8,9 @@ public class Astroid : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {        
+    {
+
+
     }
 
     // Update is called once per frame
