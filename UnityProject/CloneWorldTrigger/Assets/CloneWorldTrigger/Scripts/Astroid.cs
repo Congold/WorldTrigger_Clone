70eb4c509abfbf5ad709aa8040fb92b0b87be888
kazushi sakashita <kazusi@hotmail.co.jp>
2019-02-11 02:39:15 +0900

    [update] アステロイドのprefabから弾部分を分割

--- UnityProject/CloneWorldTrigger/Assets/CloneWorldTrigger/Scripts/Astroid.cs
+++ UnityProject/CloneWorldTrigger/Assets/CloneWorldTrigger/Scripts/Astroid.cs
@@ -4,12 +4,30 @@ using UnityEngine;
 
 public class Astroid : MonoBehaviour
 {
-    [SerializeField] private float power;
+    [SerializeField] private float _power; //威力
+    [SerializeField] private int _part; //分割数
+    [SerializeField] private GameObject _cubePrefab; //単体アステロイドプレハブ
+
+    private void OnDrawGizmos()
+    {
+        //ギズモの色を変更
+        Gizmos.color = new Color32(145, 244, 139, 210);
+        Gizmos.DrawWireCube(transform.position, transform.lossyScale);
+    }
 
     // Start is called before the first frame update
     void Start()
     {
-
+        float s = 1.0f / _part;
+        int pHalf = _part / 2;
+        var vs = new Vector3(s, s, s);
+        float firstPoint = -1 * s * pHalf + ((_part % 2 != 0) ? 0 : s / 2);
+        for (float i = firstPoint; i<=s*pHalf; i+=s)
+        {
+            Debug.Log(i);
+            var o = Instantiate(_cubePrefab, new Vector3(0, 0, i),Quaternion.identity, transform);
+            o.transform.localScale = vs;
+        }
 
     }
 
